4e1b59d5482c0c0151d3df1eecfdeb67e0afe4e9
Elin Forsberg <elinfsp96@hotmail.com>
2023-04-11 13:56:22 +0200

    menu börjat

--- front-page.php
+++ front-page.php
@@ -1,22 +1,37 @@
-<hi>hej från front-page-php</hi>
+<!-- <hi>hej från front-page-php</hi> -->
 <?php get_header(); ?>
 
 		<main>
+            <?php
+            while (have_posts()){
+                the_post(); ?>
+            
 			<section>
 				<div class="container">
 					<div class="row">
 						<div class="col-xs-12">
 							<div class="hero">
-								<img src="img/city.jpg" />
+								<!-- <img src="img/city.jpg" /> -->
+                                <?php
+                                the_post_thumbnail();
+                                ?>
 								<div class="text">
-									<h1>Hej och välkommen!</h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex.</p>
+									<!-- <h1>Hej och välkommen!</h1> -->
+                                    <h1>
+                                        <?php the_title(); ?>
+                                    </h1>
+									<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex.</p> -->
+                                    <p>
+                                        <?php the_content(); ?>
+                        
+                                    </p>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</section>
+            <?php }?>
 		</main>
-
+            
 		<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,12 +1,19 @@
 <?php
-// function theme_scripts(){
-    wp_enqueue_style('style', get_template_directory_uri().'/template_files/css/style.css');
-    wp_enqueue_style('style', get_template_directory_uri().'/template_files/css/font-awsome.css');
-    wp_enqueue_style('style', get_template_directory_uri().'/template_files/css/bootstrap.css',);
+function theme_scripts(){
+   wp_enqueue_style('bootstrap', get_template_directory_uri().'/template_files/css/bootstrap.css');
+   wp_enqueue_style('style', get_template_directory_uri().'/template_files/css/style.css');
+   wp_enqueue_style('font-awesome', get_template_directory_uri().'/template_files/css/font-awesome.css');
 
+}
+    add_action('wp_enqueue_scripts','theme_scripts');
+    
+    add_theme_support('post-thumbnails');
 
-//     wp_enqueue_scripts('scripts', get_template_directory_uri().'template_files/js/script.js');
-// }
-// add_action('wp_enqueue_scripts', 'theme_scripts');
-// 
+    function register_my_menu(){
+        register_nav_menu('huvudmeny', 'Huvudmeny');
+    }
+    
+    add_action('after_setup_theme', 'register_my_menu');
+
+    add_theme_support('menus');
 ?>
